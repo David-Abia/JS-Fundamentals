69314c49f850ca46541d9d07aed18cf94beb8246
David-Abia <abiadavid534@gmail.com>
2026-01-21 13:59:34 +0100

    correction made

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,6 +1,4 @@
-const myVar1 = "C is fun"
-const myVar2 = "Python is cool"
-const myVar3 = "JavaScript is amazin"
-console.log(myVar1)
-console.log(myVar2)
-console.log(myVar3)
+
+console.log( "C is fun")
+console.log("Python is cool")
+console.log("JavaScript is amazin")
