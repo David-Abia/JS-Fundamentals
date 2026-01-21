dfee9d13373ac03fe61a81f007d1e0dd4739384f
David-Abia <abiadavid534@gmail.com>
2026-01-21 16:42:24 +0100

    second commit

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,11 @@
-const linesOfScript = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-for(let i = 0; i < linesOfScript.length; i++) {
-    console.log(linesOfScript[i]);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+
+for (let i = 0; i < lines.length; i++) {
+  output += lines[i];
+  if (i !== lines.length - 1) {
+    output += '\n';
+  }
 }
+
+console.log(output);
